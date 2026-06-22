f4a345a19317aa6da5f57a15afedb0e079b6882d
ikhsan <m_ikhsan20@mhs.usk.ac.id>
2026-06-22 13:29:49 +0000

    Remove unused FeatureGrid and AnnouncementsSection components from Home page

--- app/page.tsx
+++ app/page.tsx
@@ -1,17 +1,13 @@
 import Navbar from "./components/Navbar";
 import Hero from "./components/Hero";
-import FeatureGrid from "./components/FeatureGrid";
 import AppGrid from "./components/AppGrid";
 import Footer from "./components/Footer";
-import AnnouncementsSection from "./components/AnnouncementsSection";
 
 export default function Home() {
   return (
     <>
       <Navbar />
       <Hero />
-      <FeatureGrid />
-      <AnnouncementsSection />
       <AppGrid />
       <Footer />
     </>
